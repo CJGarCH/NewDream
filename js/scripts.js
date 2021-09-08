2e16eaa72878e967731deba4fbe810a68c7982c9
Camilo José García Chacón <camilogarciach12@gmail.com>
2021-09-08 11:02:09 -0500

    first coomit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#leBtn").click(function(){
+      $("div").filter("#venta").toggle(1000);
+    });
+  });
+  $(document).ready(function(){
+    $("#leBtn2").click(function(){
+      $("div").filter("#arriendo").toggle(1000);
+    });
+});
+
+$('select[id="dDown"]').change(function() {
+ // alert();
+ if($(this).val() == "2"){
+/*    alert($(this).val()) */
+   $("#man").attr("src","images/mujer.png")
+ }else{
+  $("#man").attr("src","images/hombre.png")
+ }
+ 
+ 
+
+});
+/* $('select[id="dDown"]').change(function(){
+
+  if ($(this).val() == "2"){
+      alert("call the do something function on option 2");
+   }        
+});​ */
